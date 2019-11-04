7de0ef78fd43901e1d268fe16000579ba2933ab0
Cizeur <cgiron@e1r3p15.42.fr>
2019-11-04 19:45:02 +0100

    finished

---
+++ day00/ex10/loading.py
@@ -0,0 +1,33 @@
+from time import sleep
+import time
+import sys
+
+def ft_progress(mylist:list):
+    length = len(mylist)
+    start=time.time()
+    eta = 0
+    updated = 0
+    if length:
+        for key, val in enumerate(mylist):
+            key += 1
+            current = time.time()-start
+            percent =  key * 100 // length
+            avancement = percent // 5 * "=" + ">"
+            if not (percent % 5) and updated:
+                eta = current * 100 / percent
+                updated = 0
+            if percent % 5:
+                updated = 1
+            print("{: <80s}\r".format(" "), end="")
+            print("ETA:{: .2f}s [{:3d}%] [{:21s}] {:d}/{:d} |elapsed time {:.2f}s\r".format(eta, percent, avancement, key, length , current), end="")
+            yield val
+        print("")
+
+if __name__ == "__main__":
+    listy = range(100)
+    ret = 0
+    for elem in ft_progress(listy):
+        ret += (elem + 3) % 5
+        sleep(0.01)
+    print()
+    print(ret)
