1e51affd5246e906672e47104181a62d10c8ea62
Cizeur <cgiron@e1r5p17.42.fr>
2019-11-05 17:43:50 +0100

    custom sort and shuffle

--- day01/ex03/generator.py
+++ day01/ex03/generator.py
@@ -1,4 +1,5 @@
-import random
+def custom_sort(string):
+    return (string.swapcase())
 
 def generator(text, sep = "", option = None):
     """Option is an optional arg, sep is mandatory"""
@@ -12,9 +13,14 @@ def generator(text, sep = "", option = None):
     if option == "unique":
         output = list(dict.fromkeys(output))
     elif option == "ordered":
-        output.sort()
+        output.sort(key = custom_sort)
     elif option == "shuffle":
-        random.shuffle(output)
+        i = 1
+        while i <= 10 :
+            stock = output
+            output = [x for i,x in enumerate(stock) if i % 2]
+            output += [x for i,x in enumerate(stock) if not i % 2]
+            i += 1
     elif option:
         yield ("ERROR")
         return
@@ -22,16 +28,20 @@ def generator(text, sep = "", option = None):
         yield elm
     
 if __name__ == '__main__':
-    text = "Le Lorem Ipsum simplement est simplement du faux texte. Le Lorem est"
-    print ("test")
+    text = "Le Lorem Ipsum est simplement du faux texte. Le Lorem est une base de simplemet"
+  #  text = "1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18"
+    print ("######test######")
     for word in generator(text, sep=" "):
         print(word)
-    print ("test_shuffle")
+    print ("######test_shuffle######")
     for word in generator(text, sep=" ", option="shuffle"):
         print(word)
-    print ("test_unique")
+    print ("######test_ordered######")
+    for word in generator(text, sep=" ", option="ordered"):
+        print(word)
+    print ("######test_unique######")
     for word in generator(text, sep=" ", option="unique"):
         print(word)
-    print ("test error")
+    print ("######test error######")
     for word in generator("hello", sep=" ", option="uniqu"):
         print(word)
