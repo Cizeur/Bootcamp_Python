31b8542e67caa4533e4223d96900348c86076b93
Cizeur <cgiron@e1r5p17.42.fr>
2019-11-05 17:43:15 +0100

    eval seems to be done

---
+++ day01/ex04/eval.py
@@ -0,0 +1,41 @@
+class Evaluator:
+    """String weight evaluator"""
+
+    @staticmethod
+    def check_args(coefs, words):
+        if len(coefs) != len(words):
+            return(False)
+        if not isinstance(coefs, list):
+            return(False)
+        if not isinstance(words, list):
+            return(False)
+        try:
+            coefs = list(map(float,coefs))
+        except Exception:
+            return(False)
+        if not all(isinstance(x, str) for x in words):
+            return(False)
+        return(True)
+
+        
+    @staticmethod
+    def zip_evaluate(coefs, words):
+        if not Evaluator.check_args(coefs,words):
+            return(-1)
+        zipped = zip(coefs, words)
+        return(sum([x[0] * len(x[1]) for x in zipped]))
+            
+    @staticmethod
+    def enumerate_evaluate(coefs, words):
+        if not Evaluator.check_args(coefs,words):
+            return(-1)
+        enumerated = [ coef * len(words[i]) for i, coef in enumerate(coefs)]
+        return(sum(enumerated))
+    
+if __name__ == '__main__':
+    words = ["Le", "Lorem", "Ipsum", "est", "simple"]
+    coefs = [1.0, 2.0, 1.0, 4.0, 0.5]
+   # words = ["Le", "Lorem", "Ipsum", "n'", "est", "pas", "simple"]
+   # coefs = [0.0, -1.0, 1.0, -12.0, 0.0, 42.42]
+    print(Evaluator.zip_evaluate(coefs, words))
+    print(Evaluator.enumerate_evaluate(coefs, words))
