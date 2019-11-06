741ed6ded22edcf0b14e6af3276579e4f67ff244
Cizeur <cgiron@e1r6p15.42.fr>
2019-11-06 20:47:55 +0100

    it works for God sake

---
+++ day02/ex04/build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+rm -rf ai42
+mkdir -p ai42
+mkdir -p ai42/logging
+cp log.py ./ai42/logging/
+cp progressbar.py ./ai42/
+echo "import progressbar, logging" >./ai42/__init__.py
+echo "import log" > ./ai42/logging/__init__.py
+cat > setup.py << End
+# import setup function from  
+# python distribution utilities 
+ 
+from setuptools import setup, find_packages
+
+# Calling the setup function 
+setup( 
+      name = 'ai42', 
+      version = '1.0.0',
+      packages=find_packages(),
+      author ='Cesar', 
+      author_email = 'None of your business', 
+      url = 'https://github.com/Cizeur', 
+      description = 'basic lib', 
+      keywords='basic', 
+) 
+End
+python setup.py sdist
+rm setup.py
+rm -rf setup.py
+rm -rf ai42
---
+++ day02/ex04/progressbar.py
@@ -0,0 +1,27 @@
+from time import sleep
+import time
+
+
+def ft_progress(mylist: list):
+    length = len(mylist)
+    start = time.time()
+    eta = 0
+    updated = 0
+    if length:
+        for key, val in enumerate(mylist):
+            key += 1
+            current = time.time()-start
+            percent = key * 100 // length
+            avancement = percent // 5 * "=" + ">"
+            if not (percent % 5) and updated:
+                eta = current * 100 / percent
+                updated = 0
+            if percent % 5:
+                updated = 1
+            print("{: <80s}\r".format(" "), end="")
+            print("ETA:{:.2f}s [{:3d}%] [{:21s}]"
+                  "{:d}/{: d} |elapsed time {:.2f}s\r"
+                  .format(eta, percent, avancement, key, length, current),
+                  end="")
+            yield val
+        print("")
