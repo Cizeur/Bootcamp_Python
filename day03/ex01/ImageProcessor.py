86c87ee2ab1d9441e35b5977e77940365152ad1a
Cizeur <cgiron@e1r6p15.42.fr>
2019-11-07 12:10:24 +0100

    done

---
+++ day03/ex01/ImageProcessor.py
@@ -0,0 +1,23 @@
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+class ImageProcessor():
+
+    def load(self, path):
+        arr = mpimg.imread(path)
+        print("Loading image of dimensions {} x {}".format(
+            (*(arr.shape[0:2]))))
+        return arr
+
+    def display(self, array):
+        plt.imshow(array)
+        plt.show()
+
+
+if __name__ == "__main__":
+    imp = ImageProcessor()
+    arr = imp.load("./42AI.png")
+    print(arr)
+    imp.display(arr)
