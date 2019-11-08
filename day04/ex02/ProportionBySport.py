bc8726d730552a456545b3eccfbd029a7af5df68
Cizeur <cgiron@e1r6p15.42.fr>
2019-11-08 19:48:54 +0100

    multiple ids for athletes

--- day04/ex02/ProportionBySport.py
+++ day04/ex02/ProportionBySport.py
@@ -2,14 +2,14 @@ def proportionBySport(df, o_year, sport, gender):
     df = df[['ID','Name','Sex', 'Year', 'Sport']]
     df = df.drop(df[df.Sex != "F" ].index)
     df = df.drop(df[df.Year != o_year ].index)
+    df.drop_duplicates(subset ="Name", inplace = True)
     df.drop_duplicates(subset ="ID", inplace = True)
     total = df.shape[0]
     df = df.drop(df[df.Sport != sport ].index)
-    print (df.shape[0]/total)
-    return (df)
+    return (df.shape[0]/total)
 
 if __name__ == "__main__":
     from FileLoader import FileLoader
     loader = FileLoader()
     data = loader.load("../ressources/athlete_events.csv")
-    proportionBySport(data, 2004, 'Tennis', 'F')
+    print(proportionBySport(data, 2004, 'Tennis', 'F'))
