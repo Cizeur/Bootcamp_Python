2db63146a2278425fb8303f2cfaf8d8aa92b9781
Cizeur <cgiron@e1r6p15.42.fr>
2019-11-08 19:49:28 +0100

    To the norm

--- day04/ex02/ProportionBySport.py
+++ day04/ex02/ProportionBySport.py
@@ -1,12 +1,13 @@
 def proportionBySport(df, o_year, sport, gender):
-    df = df[['ID','Name','Sex', 'Year', 'Sport']]
-    df = df.drop(df[df.Sex != "F" ].index)
-    df = df.drop(df[df.Year != o_year ].index)
-    df.drop_duplicates(subset ="Name", inplace = True)
-    df.drop_duplicates(subset ="ID", inplace = True)
+    df = df[['ID', 'Name', 'Sex', 'Year', 'Sport']]
+    df = df.drop(df[df.Sex != "F"].index)
+    df = df.drop(df[df.Year != o_year].index)
+    df.drop_duplicates(subset="Name", inplace=True)
+    df.drop_duplicates(subset="ID", inplace=True)
     total = df.shape[0]
-    df = df.drop(df[df.Sport != sport ].index)
+    df = df.drop(df[df.Sport != sport].index)
     return (df.shape[0]/total)
+
 
 if __name__ == "__main__":
     from FileLoader import FileLoader
