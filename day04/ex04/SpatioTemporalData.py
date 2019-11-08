fd9c0bffc5b9c749d32a048feea0001a6297419c
Cizeur <cgiron@e1r6p15.42.fr>
2019-11-08 14:18:31 +0100

    Feel like a time traveler

---
+++ day04/ex04/SpatioTemporalData.py
@@ -0,0 +1,27 @@
+class SpatioTemporalData():
+
+    def __init__(self, data):
+        self._data = data
+
+    def where(self, year):
+        df = self._data
+        df = df.drop(df[df.Year != year ].index)
+        df.drop_duplicates(subset ="City", inplace = True)
+        return (df["City"].tolist())
+
+    def when(self, location):
+        df = self._data
+        df = df.drop(df[df["City"] != location ].index)
+        df.drop_duplicates(subset ="Year", inplace = True)
+        return (df["Year"].tolist())
+
+
+if __name__ == "__main__":
+    from FileLoader import FileLoader
+    loader = FileLoader()
+    data = loader.load("../ressources/athlete_events.csv")
+    sp = SpatioTemporalData(data)
+    print(sp.where(1896))
+    print(sp.where(2016))
+    print(sp.when('Athina'))
+    print(sp.when('Paris'))
